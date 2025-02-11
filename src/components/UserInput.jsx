0eb09f222028c036455a0b2dcb22f22dd2b23477
Hrasek <hosekmotor@seznam.cz>
2025-02-11 09:15:38 +0100

    feat: Added dynamic investment calculation.

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,8 +1,24 @@
-export default function UserInput({ label }) {
+import { useState } from "react";
+
+export default function UserInput({ label, onChangeInput, name }) {
+  const [value, setValue] = useState("");
+
+  function handleInputChange(event) {
+    let newValue = Number(event.target.value);
+    newValue = Math.abs(newValue);
+    setValue(newValue);
+    onChangeInput(name, newValue);
+  }
   return (
     <p>
       <label>{label}</label>
-      <input type="number" required />
+      <input
+        type="number"
+        required
+        min="0"
+        value={value}
+        onChange={handleInputChange}
+      />
     </p>
   );
 }
